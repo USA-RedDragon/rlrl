7d42672692590f454a2978e799f0ee1a37dabcf4
Jacob McSwain <jacob@mcswain.dev>
2026-01-12 08:59:45 -0600

    adjust sweep params

--- rlrl/config/sweeps/hyperparameters.py
+++ rlrl/config/sweeps/hyperparameters.py
@@ -3,10 +3,10 @@ config = {
     "metric": {"name": "Policy Reward", "goal": "maximize"},
     "parameters": {
         "random_seed": {"values": [1, 2, 3]},
-        "exp_buffer_size": {"min": 100_000, "max": 50_000_000 },
+        "exp_buffer_size": {"min": 100_000, "max": 3_000_000 },
         "ts_per_iteration": {"min": 5, "max": 50_000 },
-        "sac_batch_size": {"min": 128, "max": 5_000_000 },
-        "sac_max_updates_per_iter": {"min": 500, "max": 5_000_000 },
+        "sac_batch_size": {"min": 128, "max": 50_000 },
+        "sac_max_updates_per_iter": {"min": 500, "max": 50_000 },
         "sac_ent_coef": {"min": 0.01, "max": 0.2},
         "sac_learning_rate": {"min": 1e-4, "max": 1e-3},
         "sac_learning_starts": {"min": 5_000, "max": 200_000},
