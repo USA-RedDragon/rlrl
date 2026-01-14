dafc3bdf2e2020189de3d1461b3b3535bb5edc51
Jacob McSwain <jacob@mcswain.dev>
2026-01-13 22:00:50 -0600

    arial: adjust reward weights

--- rlrl/config/aerial.py
+++ rlrl/config/aerial.py
@@ -9,29 +9,29 @@ from rlrl.consts import GAME_TICK_RATE, TICK_SKIP
 config = {
     "rewards": [
         ("event", EventReward(
-               touch=10,
-               team_goal=25,
-               concede=-25,
-               boost_pickup=8,
-               demo=8,
-            ), 1),
-        ("vel_player_to_ball", VelocityPlayerToBallReward(), 5),
+            touch=10,
+            team_goal=25,
+            concede=-25,
+            boost_pickup=8,
+            demo=8,
+        ), 1),
+        ("vel_player_to_ball", VelocityPlayerToBallReward(), 4),
         ("vel_ball_to_goal", VelocityBallToGoalReward(), 8),
         ("save_boost", SaveBoostReward(), 2),
         ("aerial_touch_impulse", AerialTouchImpulseReward(
-               min_impulse=200,
-               impulse_scale=0.005
-            ), 1),
+            min_impulse=200,
+            impulse_scale=0.005
+        ), 1),
         ("simple_aerial", SimpleAerialReward(), 8),
         ("air_time", AirTimeReward(
             time_scale=0.02
         ), 4),
         ("air_height", AirHeightReward(
             height_scale=1.0
-        ), 4),
+        ), 2),
         ("vel_player_to_boost", VelocityPlayerToBoostReward(
-               min_boost=0.5
-            ), 4),
+            min_boost=0.7
+        ), 4),
     ],
     "terminal_conditions": [
         GoalScoredCondition(),
